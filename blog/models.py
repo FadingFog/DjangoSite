9cfadf113148b1329c21a0bd71ab7cece8560de4
FadingFog <aleksey.guzlov@gmail.com>
2021-02-15 23:04:43 +0300

    Oleg Molchanov guide part 4

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,23 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.shortcuts import reverse
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=150, db_index=True)
+    text = models.TextField(blank=True, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from blog.views import HotelListView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('hotels/', HotelListView.as_view()),
+    path('post/<str:slug>/', views.post_detail, name='post_detail'),
+    path('hotels/', HotelListView.as_view(), name='hotel_list'),
 ]
